fc0c9ce74be3e0aeae88d833be938cdd471de21b
Stephane Lefebvre <stephane.epiphane.lefebvre@gmail.com>
2023-09-01 06:28:10 +0200

    initial commit

---
+++ TrialGA/TrialGAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialGAApp.swift
+//  TrialGA
+//
+//  Created by Stephane Lefebvre on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TrialGAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
